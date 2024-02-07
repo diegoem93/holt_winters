9e999ce505873b414997b4cfe7cb155cb2ce3b6d
diegoem93 <diegoem93@gmail.com>
2024-02-07 08:17:25 -0800

    developed all modules

--- modeling/exponential_smoothing.py
+++ modeling/exponential_smoothing.py
@@ -2,10 +2,13 @@ from statsmodels.tsa.holtwinters import SimpleExpSmoothing
 import os
 import sys
 import pandas as pd
+import warnings
+from statsmodels.tools.sm_exceptions import ConvergenceWarning
+warnings.simplefilter('ignore', ConvergenceWarning)
 
 path = os.getcwd()
 parent_path = os.path.abspath(os.path.join(path, os.pardir))
-data_path = str(parent_path) + "/Forecastor/data_processing"
+data_path = str(parent_path) + "/holt_winters/data_processing"
 
 sys.path.append(data_path)
 
@@ -13,22 +16,11 @@ sys.path.append(data_path)
 from data_handler import get_data, get_stores, get_families, get_time_series
 
 # single exponential smoothing
-def exp_smoothing_forecast(data):
+def exp_smoothing_forecast(data, forecast_days):
     # create class
     model = SimpleExpSmoothing(data)
     # fit model
     model_fit = model.fit()
     # make prediction
-    yhat = model_fit.forecast(6)
+    yhat = model_fit.forecast(forecast_days)
     return yhat
-
-df = get_data()
-print(df.head())
-table = pd.pivot_table(df, values='sales', index=['store_nbr', 'family'], aggfunc="sum")
-print(table.head())
-df_time_serie = df[(df['family']=="BEVERAGES")&(df['store_nbr']==1)]
-df_time_serie = df_time_serie[['date', 'sales']]
-df_time_serie = df_time_serie.set_index('date')
-print(df_time_serie)
-yhat = exp_smoothing_forecast(df_time_serie)
-print(yhat)
